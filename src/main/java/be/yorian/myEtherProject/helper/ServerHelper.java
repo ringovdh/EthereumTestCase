d3e02a7a432b9d4c1406a3dcc46afd07f47418bf
Van den Hende Ringo <ringo@192.168.0.101>
2018-03-17 11:37:30 +0100

    init commit

---
+++ src/main/java/be/yorian/myEtherProject/helper/ServerHelper.java
@@ -0,0 +1,13 @@
+package be.yorian.myEtherProject.helper;
+
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+
+public class ServerHelper {
+
+	public Web3j startserver() {
+		Web3j web3 = Web3j.build(new HttpService());  // defaults to http://localhost:8545/
+		return web3;
+	}
+
+}
